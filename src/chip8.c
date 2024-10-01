a60951a4e6ae790a9d9af1e466520e171347b92c
Sam Slater <sslater@posteo.net>
2024-10-01 02:17:37 +0000

    Remove inline comment from preprocessor macro

--- src/chip8.c
+++ src/chip8.c
@@ -4,7 +4,8 @@
 #include <string.h>
 #include <time.h>
 
-#define CHIP8_FONTSET_START_ADDRESS 0x50 // CHIP-8 fontset (contains hexadecimal digits 0-F, stored at memory locations 0x050 to 0x09F)
+// CHIP-8 fontset (contains hexadecimal digits 0-F, stored at memory locations 0x050 to 0x09F)
+#define CHIP8_FONTSET_START_ADDRESS 0x50 
 #define CHIP8_FONTSET_SIZE 80
 #define CHIP8_MEMORY_SIZE 4096 
 #define CHIP8_NUM_REGISTERS 16
