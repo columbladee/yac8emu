3687bf3597fa9046586846359d0b7339b7b5b330
Sam Slater <sslater@posteo.net>
2024-10-05 23:10:50 +0000

    Add timer.c

---
+++ src/timer.c
@@ -0,0 +1,18 @@
+#include "chip8.h"
+#include "sdl_wrapper.h" //for playSound()
+
+//Function to update the delay and sound timers
+void updateTimers(chip8_t *chip8) {
+	// Decrement delay timer if > 0
+	if (chip8->delay_timer > 0) {
+		chip8->delay_timer--;
+	}
+
+	// Decrement sound timer if > 0 and playSound() if = 0
+	if (chip8->sound_timer > 0) {
+		chip8->sound_timer--;
+	}
+	if (chip8->sound_timer == 0) {
+		playSound();
+	}
+}
