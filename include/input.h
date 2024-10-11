4072f9bc2a18fa58dd92b882ca28adfa6529b4cf
Sam Slater <sslater@posteo.net>
2024-10-11 01:20:40 +0000

    Refactor input.h

---
+++ include/input.h
@@ -0,0 +1,12 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+#include "chip8.h"
+#include <stdbool.h>
+
+// Prototype input handling function
+
+
+void handleInput(chip8_t *chip8, bool *running);
+
+#endif // INPUT_H
