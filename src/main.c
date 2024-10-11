172de8c15b78d7f9beb6669c4a26e7b32bee0670
Sam Slater <sslater94@bitbucket.com>
2024-10-11 14:37:00 -0700

    Add up to main emulation loop in main.c

--- src/main.c
+++ src/main.c
@@ -1,4 +1,4 @@
-#include "chip8.h"
+include "chip8.h"
 #include "graphics.h"
 #include "input.h"
 #include "audio.h"
@@ -10,5 +10,37 @@
 #include <stdbool.h>
 
 int main(int argc, char **argv) {
-    
+	if (argc != 2) {
+		printf("Usage: %s <ROM_FILE>\n", argv[0]);
+		return EXIT_FAILURE;
+	}
+	
+	// Init logger, SDL, Graphics, Audio, CHIP-8 instance
+
+	initializeLogger("logs/chip8_emulator.log");
+	logInfo("CHIP-8 Emulator started");
+
+	if (initializeSDL() != 0) {
+		logError("Failed to initialize SDL");
+		closeLogger();
+		return EXIT_FAILURE;
+	}
+
+	if (initializeGraphics() != 0) {
+		logError("Failed to initialize graphics");
+		destroySDL();
+		closeLogger();
+		return EXIT_FAILURE;
+	}
+
+	initializeAudio();
+
+	chip8_t chip8;
+	initializeCPU(&chip8)
+	loadROM(&chip8, argv[1]);
+	
+
+	// Main emulation loop
+
+
 }
